727fed5f269587a90d859cc629356594dd2d4d66
Vikas <vikas@simpragma.com>
2013-06-13 16:59:17 +0530

    Implemented junit test method

--- src/test/TestPrime.java
+++ src/test/TestPrime.java
@@ -2,13 +2,18 @@ package test;
 
 import static org.junit.Assert.*;
 
+import main.com.simpragma.primenumber.PrimeNum;
+
 import org.junit.Test;
 
 public class TestPrime {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+//		fail("Not yet implemented");
+		PrimeNum tester = new PrimeNum();
+//		assertFalse("Assertion failed", tester.isItPrime(7));
 	}
 
 }
