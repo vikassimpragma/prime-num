bc9991e6b58640cd10a5cf23962a47351bb6eba3
Vikas <vikas@simpragma.com>
2013-06-17 18:50:09 +0530

    added guava

--- src/main/java/com/simpragma/primenumber/Main.java
+++ src/main/java/com/simpragma/primenumber/Main.java
@@ -1,13 +1,22 @@
 package main.java.com.simpragma.primenumber;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import main.java.com.simpragma.primenumber.PrimeNum;
+import com.google.common.collect.*;
 
 public class Main {
 	public static void main(String[] args) {
+		Multiset<String> wordsMultiset = HashMultiset.create();
+		Collection<String> words = new HashSet<String>();
+		words.add("Sunday");words.add("Monday");
+		wordsMultiset.addAll(words);
 		Integer i = 5;
 		while (true) {
 			PrimeNum p = new PrimeNum();
 			System.out.println("isItPrime(" + i + ") returned " + p.isItPrime(i).toString());
+			System.out.println("guava "+wordsMultiset.count(java.lang.Object.class));
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
