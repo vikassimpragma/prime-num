3e255d3168a57fd56037b49d05a5f70514ea7e02
Vikas <vikas@simpragma.com>
2013-06-17 12:54:58 +0530

    added infinite loop in Main.java

--- src/main/java/com/simpragma/primenumber/Main.java
+++ src/main/java/com/simpragma/primenumber/Main.java
@@ -5,7 +5,15 @@ import main.java.com.simpragma.primenumber.PrimeNum;
 public class Main {
 	public static void main(String[] args) {
 		Integer i = 5;
-		PrimeNum p = new PrimeNum();
-		System.out.println(p.isItPrime(i).toString());
+		while (true) {
+			PrimeNum p = new PrimeNum();
+			System.out.println("isItPrime(" + i + ") returned " + p.isItPrime(i).toString());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
